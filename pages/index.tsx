c9a910db9b0b9343bea8a7801f3dd5f0554d1dbc
Luke Dearnley <luked@phm.gov.au>
2020-12-07 18:24:21 +1100

    morre fiddling with redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-const HomePage = () => {
-  return null;
-};
+import React from 'react';
+import Head from 'next/head';
 
-export const getServerSideProps = async ({ req, res }) => {
-  if (res) {
-    res.writeHead(302, {
-      // or 301
-      Location: '/art-index',
-    });
-    res.end();
-  }
-};
+// const HomePage = () => {
+//   return null;
+// };
 
-export default HomePage;
+export default function HomePage() {
+  return (
+    <Head>
+      <title>-</title>
+      <meta httpEquiv="refresh" content={`0;url=/art-index`} />
+    </Head>
+  );
+}
+
+// export const getServerSideProps = async ({ req, res }) => {
+//   if (res) {
+//     res.writeHead(302, {
+//       // or 301
+//       Location: 'art-index',
+//     });
+//     res.end();
+//   }
+// };
+
+// export default HomePage;
