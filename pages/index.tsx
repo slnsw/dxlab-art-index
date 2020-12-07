afb306fa07eaf8c66a760082e8603e610d9cd2c1
Luke Dearnley <luked@phm.gov.au>
2020-12-07 18:16:09 +1100

    more fiddling with redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export const getServerSideProps = async ({ req, res }) => {
   if (res) {
     res.writeHead(302, {
       // or 301
-      Location: 'art-index/',
+      Location: '/art-index',
     });
     res.end();
   }
