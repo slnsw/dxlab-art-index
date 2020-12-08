1ff1122d55498a19b0e241fbc429d80a4bdb9991
Luke Dearnley <luked@phm.gov.au>
2020-12-08 14:54:03 +1100

    removed some more console log

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -2,11 +2,7 @@
 import TagManager from 'react-gtm-module';
 
 export const initGA = () => {
-  console.log('GTM_ID', process.env.GTM_ID);
-
   if (process.env.GTM_ID) {
-    console.log('GTM_ID', 'Init');
-
     const tagManagerArgs = {
       gtmId: process.env.GTM_ID,
     };
