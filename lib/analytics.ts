96955bb110ec28096b4aa96e018a778b11a62c21
Kaho Cheung <kaho@elefanttraks.com>
2020-12-08 14:36:01 +1100

    Add logs to initGA

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -2,7 +2,11 @@
 import TagManager from 'react-gtm-module';
 
 export const initGA = () => {
+  console.log('GTM_ID', process.env.GTM_ID);
+
   if (process.env.GTM_ID) {
+    console.log('GTM_ID', 'Init');
+
     const tagManagerArgs = {
       gtmId: process.env.GTM_ID,
     };
